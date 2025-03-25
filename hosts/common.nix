e3046d093571a66f05016df271d9cf6bebee4851
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-25 12:09:00 -0700

    mount /tmp in ram

--- hosts/common.nix
+++ hosts/common.nix
@@ -89,4 +89,13 @@ in
       "d ${syncthingDir} 0770 syncthing syncthing"
     ];
   };
+
+  fileSystems."/tmp" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [
+      "mode=1777"
+      "size=4G"
+    ];
+  };
 }

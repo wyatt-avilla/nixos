a70712f76175de858687d35d3408fdde36742221
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-08 17:37:05 -0700

    import homelab config

--- flake.nix
+++ flake.nix
@@ -4,10 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
+
+    nix-secrets = {
+      url = "git+ssh://git@github.com/wyatt-avilla/nix-secrets";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }:
+    inputs@{
+      self,
+      nixpkgs,
+      nix-secrets,
+      ...
+    }:
     let
       system = "x86_64-linux";
 
@@ -30,6 +40,8 @@
 
           modules = [ ./hosts/desktop/configuration.nix ];
         };
+
+        homelab = nixpkgs.lib.nixosSystem { modules = [ ./hosts/homelab/configuration.nix ]; };
       };
 
       devShells.${system}.default = pkgs.mkShell {
---
+++ hosts/homelab/configuration.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "barachiel";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Los_Angeles";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  users.users.wyatt = {
+    isNormalUser = true;
+    description = "Wyatt Avilla";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [ ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+  ];
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "24.11";
+}

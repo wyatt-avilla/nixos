8b33fc5df285509c45e5dbb1f597f966c45eec31
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-12 02:45:38 -0700

    change perms

--- hosts/homelab/storage.nix
+++ hosts/homelab/storage.nix
@@ -20,8 +20,8 @@
 
     systemd = {
       tmpfiles.rules = [
-        "d ${config.storageDir} 0755 root storage -"
-        "d ${config.storageDir}/syncthing 0760 syncthing storage -"
+        "d ${config.storageDir} 0770 root storage -"
+        "d ${config.storageDir}/syncthing 0770 syncthing storage -"
       ];
 
       services.filebrowser.serviceConfig.group = "storage";
@@ -35,10 +35,10 @@
           ExecStart = pkgs.writeShellScript "fix-storage-dir-perms" ''
             set -e
             chown root:storage "${config.storageDir}"
-            chmod 0755 "${config.storageDir}"
+            chmod 0770 "${config.storageDir}"
 
             chown syncthing:storage "${config.storageDir}/syncthing"
-            chmod 0760 "${config.storageDir}/syncthing"
+            chmod 0770 "${config.storageDir}/syncthing"
           '';
         };
       };

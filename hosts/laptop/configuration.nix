c55d5ff2861478fdc8afe813177d59f755489951
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-06-19 20:02:40 -0700

    tuigreet maybe

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,5 +1,12 @@
-{ config, pkgs, ... }:
-
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+in
 {
   imports = [
     ../../modules/common.nix
@@ -32,6 +39,26 @@
     };
 
     fprintd.enable = true;
+
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${tuigreet} --time --remember --remember-session --cmd '${lib.getExe pkgs.uwsm} start hyprland-uwsm.desktop > /dev/null'";
+          user = "greeter";
+        };
+      };
+    };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 
   system.stateVersion = "25.05";

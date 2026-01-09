c467634c3edf84e3ad46aa1c2e72468f47d3726d
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-01-09 00:29:28 -0800

    fix ssh key path

--- hosts/homelab/ssh.nix
+++ hosts/homelab/ssh.nix
@@ -1,21 +1,15 @@
-{ config, ... }:
-let
-  sshUser = "wyatt";
-in
 {
   sops.secrets.desktop-ssh-key = {
-    mode = "0400";
+    path = "/etc/ssh/authorized_keys.d/wyatt";
+    mode = "0444";
   };
 
   services.openssh = {
     enable = true;
+    authorizedKeysFiles = [ "/etc/ssh/authorized_keys.d/%u" ];
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
   };
-
-  users.users.${sshUser}.openssh.authorizedKeys.keyFiles = [
-    config.sops.secrets.desktop-ssh-key.path
-  ];
 }

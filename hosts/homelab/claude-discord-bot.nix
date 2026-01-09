c4e33334297329dda90700b6951f9539d5876328
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-01-08 23:25:22 -0800

    sops claude fix

--- hosts/homelab/claude-discord-bot.nix
+++ hosts/homelab/claude-discord-bot.nix
@@ -1,44 +1,15 @@
-{
-  config,
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  claude-discord-bot-token = "${config.variables.secretsDirectory}/claude-discord-bot-token";
-
-  tokenFile = "/var/lib/claude-discord-bot/discord-token";
-  botGroup = config.systemd.services.claude-discord-bot.serviceConfig.Group;
-  botUser = config.systemd.services.claude-discord-bot.serviceConfig.User;
-
-  copyToken = pkgs.writeShellScriptBin "copy-claude-discord-bot-token" ''
-    set -euo pipefail
-
-    echo "[Claude Discord bot setup] Copying credentials to ${tokenFile}"
-
-    install -Dm400 "${claude-discord-bot-token}" "${tokenFile}"
-
-    chown "${botUser}":"${botGroup}" "${tokenFile}"
-    echo "[Claude Discord bot setup] Credentials installed with '''${botUser}:${botGroup}''' ownership and 400 perms"
-  '';
-in
+{ config, inputs, ... }:
 {
   imports = [ inputs.claude-discord-bot.nixosModules.claude-discord-bot ];
 
-  services.claude-discord-bot = {
-    enable = true;
-    discordTokenFile = tokenFile;
+  sops.secrets.claude-discord-bot-token = {
+    owner = config.systemd.services.claude-discord-bot.serviceConfig.User;
+    group = config.systemd.services.claude-discord-bot.serviceConfig.Group;
+    mode = "0400";
   };
 
-  systemd.services.copy-discord-token = {
-    description = "Copies decrypted Claude Discord bot token into expected location";
-    wantedBy = [ "claude-discord-bot.service" ];
-    before = [ "claude-discord-bot.service" ];
-
-    serviceConfig = {
-      ExecStart = lib.getExe copyToken;
-      Type = "oneshot";
-    };
+  services.claude-discord-bot = {
+    enable = true;
+    discordTokenFile = config.sops.secrets.claude-discord-bot-token.path;
   };
 }

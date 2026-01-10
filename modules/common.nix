5f04b413e55c93f5aaea1c03c3b5ef3613f9c4f4
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-01-10 00:04:23 -0800

    add trusted users

--- modules/common.nix
+++ modules/common.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    trusted-users = [ "@wheel" ];
+  };
 
   boot = {
     tmp.cleanOnBoot = true;

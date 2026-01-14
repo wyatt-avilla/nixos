aa9c9b26bb5b053281481c4e355aca044a58c2f4
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-01-13 21:47:41 -0800

    immich oauth settings

--- hosts/homelab/immich.nix
+++ hosts/homelab/immich.nix
@@ -1,4 +1,8 @@
-{ config, ... }:
+{ inputs, config, ... }:
+let
+  user = "immich";
+  clientSecretFile = "/etc/immich/oauth-client-secret";
+in
 {
   services.immich = {
     enable = true;
@@ -6,12 +10,33 @@
     accelerationDevices = null;
     mediaLocation = "${config.storageDir}/immich";
     host = config.variables.homelab.wireguard.ip;
+    inherit user;
     inherit (config.variables.immich) port;
-    settings.server.externalDomain = "https://immich.${config.variables.domain}";
+
+    settings = {
+      server.externalDomain = "https://immich.${config.variables.domain}";
+
+      oauth = {
+        issuerURL = "https://accounts.google.com";
+        clientId = inputs.nix-secrets.nixosModules.plainSecrets.vps.oauth2-proxy-client-id;
+        clientSecret._secret = clientSecretFile;
+        buttonText = "Sign in with Google";
+        mobileOverrideEnabled = true;
+        mobileRedirectUri = "https://immich.${config.variables.domain}/api/oauth/mobile-redirect";
+      };
+    };
   };
 
   users.users.immich.extraGroups = [
     "video"
     "render"
   ];
+
+  systemd.services = config.secrets.mkCopyService {
+    name = "immich-oauth2-copy-client-secret";
+    source = "${config.variables.secretsDirectory}/oauth2-proxy-client-secret";
+    dest = clientSecretFile;
+    inherit user;
+    mode = "400";
+  };
 }

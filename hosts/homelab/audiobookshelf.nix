97e5efc698fc9859e490beef2c145b3f61ad1943
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-01-28 22:46:40 -0800

    audiobookshelf service

--- hosts/homelab/audiobookshelf.nix
+++ hosts/homelab/audiobookshelf.nix
@@ -1,1 +1,10 @@
-{ }
+{ config, ... }:
+{
+  services.audiobookshelf = {
+    enable = true;
+    port = 9981;
+    dataDir = "${config.storageDir}/audiobookshelf";
+
+    openFirewall = true; # TODO: change
+  };
+}

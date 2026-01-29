229ae28ab254571f4be7159a72307219cf0baca7
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-01-28 22:49:51 -0800

    expected dir creation oneshot service

--- hosts/homelab/audiobookshelf.nix
+++ hosts/homelab/audiobookshelf.nix
@@ -7,4 +7,14 @@
 
     openFirewall = true; # TODO: change
   };
+
+  systemd.services.audiobookshelf-setup = {
+    description = "Ensure audiobookshelf directory exists";
+    wantedBy = [ "audiobookshelf.service" ];
+    before = [ "audiobookshelf.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${config.systemd.package}/bin/systemd-tmpfiles --create";
+    };
+  };
 }

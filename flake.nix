c0077de4a12685f1ffe11724258a73d713afcb44
Wyatt Avilla <wyatt@linux.com>
2026-03-07 21:51:50 -0800

    rm disko and nixos-anywhere stuff

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     nix-secrets = {
       url = "git+ssh://git@github.com/wyatt-avilla/nix-secrets";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -33,7 +28,6 @@
       self,
       nixpkgs,
       nix-checks,
-      disko,
       ...
     }:
     let
@@ -87,7 +81,6 @@
 
           modules = [
             ./hosts/vps/configuration.nix
-            disko.nixosModules.disko
             inputs.nix-secrets.nixosModules.vps
           ];
         };
@@ -98,7 +91,6 @@
           pre-commit
           nixfmt-rfc-style
           statix
-          opentofu
         ];
         shellHook = ''
           pre-commit install
--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -1,8 +1,7 @@
-{ inputs, lib, ... }:
+{ lib, ... }:
 {
   imports = [
     ./hardware-configuration.nix
-    ./disko.nix
     ../../modules/server-common
     ../../modules/common.nix
     ./do-networking.nix
@@ -18,14 +17,7 @@
     systemd-boot.enable = lib.mkForce false;
     efi.canTouchEfiVariables = lib.mkForce false;
     grub.enable = true;
-    grub.devices = lib.mkForce [ "/dev/vda" ];
   };
-
-  # root SSH access needed for nixos-anywhere nixos-rebuild step
-  services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
-  users.users.root.openssh.authorizedKeys.keys = [
-    inputs.nix-secrets.nixosModules.plainSecrets.desktop.ssh.publicKey
-  ];
 
   system.stateVersion = "26.05";
 }

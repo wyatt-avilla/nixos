4bcd214d4824c366ea9b66c1e5f94eedd516b3d5
Wyatt Avilla <wyatt@linux.com>
2026-03-28 18:15:57 -0700

    move remainder of `storage.nix` into `default.nix`

--- hosts/homelab/storage/default.nix
+++ hosts/homelab/storage/default.nix
@@ -1,8 +1,17 @@
+{ lib, ... }:
 {
   imports = [
-    ./storage.nix
     ./backup.nix
     ./group.nix
     ./permissions.nix
   ];
+
+  options.storageDir = lib.mkOption {
+    type = lib.types.str;
+    default = "/mnt/sandisk_2tb/storage";
+  };
+
+  config = {
+    services.btrfs.autoScrub.enable = true;
+  };
 }

2a584848b209efa6a656b60756805eef303def88
Wyatt Avilla <wyatt@linux.com>
2026-04-17 01:20:19 -0700

    change ssh proxy to use jump host

--- hosts/vps/ssh-proxy.nix
+++ hosts/vps/ssh-proxy.nix
@@ -1,31 +1,27 @@
 { config, ... }:
 let
   homelabIp = config.variables.homelab.wireguard.ip;
-  vpsWgIp = config.variables.vps.wireguard.ip;
   proxyPort = 2222;
   sshPort = 22;
 in
 {
   networking.firewall.allowedTCPPorts = [ proxyPort ];
 
-  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-
-  networking.nat = {
-    enable = true;
-    internalInterfaces = [ "wg0" ];
-    externalInterface = "ens3";
-    forwardPorts = [
-      {
-        sourcePort = proxyPort;
-        destination = "${homelabIp}:${toString sshPort}";
-        proto = "tcp";
-      }
+  services.openssh = {
+    ports = [
+      sshPort
+      proxyPort
     ];
-    extraCommands = ''
-      iptables -t nat -A nixos-nat-post -d ${homelabIp} -o wg0 -j SNAT --to-source ${vpsWgIp}
-    '';
-    extraStopCommands = ''
-      iptables -t nat -D nixos-nat-post -d ${homelabIp} -o wg0 -j SNAT --to-source ${vpsWgIp} || true
+
+    extraConfig = ''
+      Match LocalPort ${toString proxyPort}
+        AllowTcpForwarding local
+        PermitOpen ${homelabIp}:${toString sshPort}
+        X11Forwarding no
+        AllowAgentForwarding no
+        PermitTunnel no
+        PermitTTY no
+        ForceCommand echo "Use this port as an SSH jump host to ${homelabIp}:${toString sshPort}."
     '';
   };
 }

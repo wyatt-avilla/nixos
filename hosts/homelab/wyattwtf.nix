6280e85238dccb1dea46636d5515ba061b19cce8
Wyatt Avilla <wyatt@linux.com>
2026-05-02 20:57:05 -0700

    update website

--- hosts/homelab/wyattwtf.nix
+++ hosts/homelab/wyattwtf.nix
@@ -1,8 +1,12 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 {
   imports = [ inputs.wyattwtf.nixosModules.wyattwtf ];
 
   services.wyattwtf = {
     enable = true;
+
+    lastfmApiKeyPath = "${config.variables.secretsDirectory}/lastfm-api-key";
+    goodreadsRssUrlPath = "${config.variables.secretsDirectory}/goodreads-rss-url";
+    letterboxdRssUrl = "https://letterboxd.com/wyattwtf/rss/";
   };
 }

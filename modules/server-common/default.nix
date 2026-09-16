c80e44cd8882c543d9ce081ed3b40ed253f4b6d9
Wyatt Avilla <wyatt@linux.com>
2026-09-15 20:04:12 -0700

    revert: use the default D-Bus implementation

--- modules/server-common/default.nix
+++ modules/server-common/default.nix
@@ -10,10 +10,6 @@
   config = {
     environment.systemPackages = with pkgs; [ btop ];
 
-    # Switching implementations requires a reboot, so keep server deploys on
-    # the implementation used before the nixpkgs 26.11 update.
-    services.dbus.implementation = "dbus";
-
     nix.gc = {
       automatic = true;
       dates = "03:15";
